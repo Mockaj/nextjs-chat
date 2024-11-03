a979a2966cc863906e873136cd4983ae9a56f5c5
Ladislav Husty <lada.husty@gmail.com>
2024-11-03 18:11:00 +0100

    Chat message indicator

--- components/prompt-form.tsx
+++ components/prompt-form.tsx
@@ -6,7 +6,7 @@ import Textarea from 'react-textarea-autosize'
 import { useActions, useUIState } from 'ai/rsc'
 
 import { UserMessage } from './stocks/message'
-import { fetchRelevantDocs} from "@/app/actions";
+import { fetchRelevantDocs } from '@/app/actions'
 import { type AI } from '@/lib/chat/actions'
 import { Button } from '@/components/ui/button'
 import { IconArrowElbow, IconPlus } from '@/components/ui/icons'
@@ -21,10 +21,10 @@ import { useRouter } from 'next/navigation'
 import { RelevantDoc } from '@/lib/types'
 
 export function PromptForm({
-                             input,
-                             setInput,
-                             setRelevantDocs // Add setRelevantDocs prop
-                           }: {
+  input,
+  setInput,
+  setRelevantDocs // Add setRelevantDocs prop
+}: {
   input: string
   setInput: (value: string) => void
   setRelevantDocs: React.Dispatch<React.SetStateAction<RelevantDoc[]>>
@@ -34,12 +34,40 @@ export function PromptForm({
   const inputRef = React.useRef<HTMLTextAreaElement>(null)
   const { submitUserMessage } = useActions()
   const [_, setMessages] = useUIState<typeof AI>()
+  const [placeholder, setPlaceholder] = React.useState(
+    'Zeptej se na právní otázku.'
+  )
+  const [isLoading, setIsLoading] = React.useState(false)
 
   React.useEffect(() => {
     if (inputRef.current) {
       inputRef.current.focus()
     }
   }, [])
+
+  React.useEffect(() => {
+    if (!isLoading) return
+
+    const cyclingMessages = [
+      'Hledám relevantní zákony...',
+      'Pročítám paragrafy...',
+      'Sestavuji odpověď...',
+      'Kontroluji správnost odpovědi...'
+    ]
+    let index = 0
+    setPlaceholder(cyclingMessages[0])
+
+    const interval = setInterval(() => {
+      index = (index + 1) % cyclingMessages.length
+      setPlaceholder(cyclingMessages[index])
+    }, 2000)
+
+    // Cleanup function
+    return () => {
+      clearInterval(interval)
+      setPlaceholder('Zeptej se na právní otázku.')
+    }
+  }, [isLoading])
 
   const handleSubmit = async (e: any) => {
     e.preventDefault()
@@ -53,75 +81,74 @@ export function PromptForm({
     setInput('')
     if (!value) return
 
-    // Optimistically add user message UI
-    setMessages(currentMessages => [
-      ...currentMessages,
-      {
-        id: nanoid(),
-        display: <UserMessage>{value}</UserMessage>
-      }
-    ])
+    setIsLoading(true)
+    try {
+      // Optimistically add user message UI
+      setMessages(currentMessages => [
+        ...currentMessages,
+        {
+          id: nanoid(),
+          display: <UserMessage>{value}</UserMessage>
+        }
+      ])
 
+      // Submit and get response message
+      const responseMessage = await submitUserMessage(value)
 
-
-    // Submit and get response message
-    const responseMessage = await submitUserMessage(value);
-
-    // Update relevant documents state on the client side
-    setRelevantDocs(responseMessage.relevantDocs);
-
-    setMessages((currentMessages) => [...currentMessages, responseMessage]);
+      // Update relevant documents state on the client side
+      setRelevantDocs(responseMessage.relevantDocs)
+      setMessages(currentMessages => [...currentMessages, responseMessage])
+    } finally {
+      setIsLoading(false)
+    }
   }
 
   return (
-      <form
-          ref={formRef}
-          onSubmit={handleSubmit}
-      >
-        <div className="relative flex max-h-60 w-full grow flex-col overflow-hidden bg-background px-8 sm:rounded-md sm:border sm:px-12">
+    <form ref={formRef} onSubmit={handleSubmit}>
+      <div className="relative flex max-h-60 w-full grow flex-col overflow-hidden bg-background px-8 sm:rounded-md sm:border sm:px-12">
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button
+              variant="outline"
+              size="icon"
+              className="absolute left-0 top-[14px] size-8 rounded-full bg-background p-0 sm:left-4"
+              onClick={() => {
+                router.push('/new')
+              }}
+            >
+              <IconPlus />
+              <span className="sr-only">New Chat</span>
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>New Chat</TooltipContent>
+        </Tooltip>
+        <Textarea
+          ref={inputRef}
+          tabIndex={0}
+          onKeyDown={onKeyDown}
+          placeholder={placeholder}
+          className="min-h-[60px] w-full resize-none bg-transparent px-4 py-[1.3rem] focus-within:outline-none sm:text-sm"
+          autoFocus
+          spellCheck={false}
+          autoComplete="off"
+          autoCorrect="off"
+          name="message"
+          rows={1}
+          value={input}
+          onChange={e => setInput(e.target.value)}
+        />
+        <div className="absolute right-0 top-[13px] sm:right-4">
           <Tooltip>
             <TooltipTrigger asChild>
-              <Button
-                  variant="outline"
-                  size="icon"
-                  className="absolute left-0 top-[14px] size-8 rounded-full bg-background p-0 sm:left-4"
-                  onClick={() => {
-                    router.push('/new')
-                  }}
-              >
-                <IconPlus />
-                <span className="sr-only">New Chat</span>
+              <Button type="submit" size="icon" disabled={input === ''}>
+                <IconArrowElbow />
+                <span className="sr-only">Send message</span>
               </Button>
             </TooltipTrigger>
-            <TooltipContent>New Chat</TooltipContent>
+            <TooltipContent>Send message</TooltipContent>
           </Tooltip>
-          <Textarea
-              ref={inputRef}
-              tabIndex={0}
-              onKeyDown={onKeyDown}
-              placeholder="Send a message."
-              className="min-h-[60px] w-full resize-none bg-transparent px-4 py-[1.3rem] focus-within:outline-none sm:text-sm"
-              autoFocus
-              spellCheck={false}
-              autoComplete="off"
-              autoCorrect="off"
-              name="message"
-              rows={1}
-              value={input}
-              onChange={e => setInput(e.target.value)}
-          />
-          <div className="absolute right-0 top-[13px] sm:right-4">
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Button type="submit" size="icon" disabled={input === ''}>
-                  <IconArrowElbow />
-                  <span className="sr-only">Send message</span>
-                </Button>
-              </TooltipTrigger>
-              <TooltipContent>Send message</TooltipContent>
-            </Tooltip>
-          </div>
         </div>
-      </form>
+      </div>
+    </form>
   )
 }
